05af8213bd084848a475b0859a45d8bcc5dd4838
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-10-13 18:42:41 -0400

    feat: create Get request to show all persons

--- src/main/java/com/java/spring/controller/PersonController.java
+++ src/main/java/com/java/spring/controller/PersonController.java
@@ -1,14 +1,20 @@
 package com.java.spring.controller;
 
 import com.java.spring.dto.PersonDto;
+import com.java.spring.exception.TokenNotFoundException;
 import com.java.spring.model.Person;
 import com.java.spring.service.PersonService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +34,13 @@ public class PersonController {
   @PostMapping("/login")
   public ResponseEntity<String> generateToken(@RequestBody PersonDto personDto) {
     return ResponseEntity.status(HttpStatus.OK).body(personService.generateToken(personDto));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Person>> findAll(
+      @RequestHeader(value="token", defaultValue = "") String token
+  ) {
+    if (token == "") throw new TokenNotFoundException();
+    return ResponseEntity.status(HttpStatus.OK).body(personService.findAll(token));
   }
 }
